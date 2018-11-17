9b524fac0432ace9cb546ec2296910efc053b4ad
Allan Wang <me@allanwang.ca>
2018-11-17 16:55:43 -0500

    Fix sql test extension

--- printer-sql/src/test/kotlin/ca/allanwang/prim/printer/sql/SqlTestHelper.kt
+++ printer-sql/src/test/kotlin/ca/allanwang/prim/printer/sql/SqlTestHelper.kt
@@ -1,17 +1,18 @@
 package ca.allanwang.prim.printer.sql
 
-import ca.allanwang.prim.printer.sql.repos.SessionTable
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.transactions.DEFAULT_ISOLATION_LEVEL
+import org.jetbrains.exposed.sql.transactions.ThreadLocalTransactionManager
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.koin.dsl.module.Module
 import org.koin.standalone.StandAloneContext.startKoin
+import kotlin.test.fail
 
 /**
  * Extension that binds additional koin modules and sql tables for each test.
@@ -25,27 +26,28 @@ open class SqlExtension(tables: List<Table>,
     private val tables = tables.toTypedArray()
 
     override fun beforeEach(context: ExtensionContext) {
-        println("BeforeAA")
         startKoin(modules)
     }
 
     override fun beforeTestExecution(context: ExtensionContext) {
-        println("BeforeEx")
-        Database.connect("jdbc:h2:mem:test;MODE=MySQL", "org.h2.Driver", "test", "test")
-        transaction {
-            debug = this@SqlExtension.debug
-            SchemaUtils.create(*tables)
-            println("Initialized 1 ${SessionTable.selectAll().count()}")
+        Database.connect("jdbc:h2:mem:test;MODE=MySQL", "org.h2.Driver", "test", "test") {
+            ThreadLocalTransactionManager(it, DEFAULT_ISOLATION_LEVEL, 1)
         }
-//        transaction {
-//            println("Test 2")
-//            println("Initialized 2 ${SessionTable.selectAll().count()}")
-//        }
+        // New transaction saves transaction to ThreadLocal; accessible through TransactionManager.currentOrNull()
+        val transaction = TransactionManager.manager.newTransaction(DEFAULT_ISOLATION_LEVEL)
+        transaction.debug = debug
+        SchemaUtils.create(*tables)
     }
 
-    override fun afterTestExecution(context: ExtensionContext) = transaction {
-        println("After")
-        SchemaUtils.drop(*tables)
+    override fun afterTestExecution(context: ExtensionContext) {
+        val transaction = TransactionManager.currentOrNull() ?: fail("No transaction manager found after sql test")
+        transaction.commit()
+        TransactionManager.resetCurrent(TransactionManager.manager)
+        transaction.currentStatement?.let {
+            if (!it.isClosed) it.close()
+            transaction.currentStatement = null
+        }
+        transaction.closeExecutedStatements()
     }
 
 }
