81f8f0ea81fa7d848e6891a52f2d77e1aa9b91a1
Allan Wang <me@allanwang.ca>
2018-11-17 17:03:18 -0500

    Init travis

--- printer-sql/src/test/kotlin/ca/allanwang/prim/printer/sql/repos/SessionTest.kt
+++ printer-sql/src/test/kotlin/ca/allanwang/prim/printer/sql/repos/SessionTest.kt
@@ -4,24 +4,10 @@ import ca.allanwang.prim.printer.repos.SessionTestBase
 import ca.allanwang.prim.printer.sql.SqlExtension
 import ca.allanwang.prim.printer.sql.sqlRepositoryModule
 import org.junit.jupiter.api.extension.ExtendWith
-import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class SessionExtension : SqlExtension(
         tables = listOf(SessionTable),
         modules = listOf(sqlRepositoryModule))
 
 @ExtendWith(SessionExtension::class)
-class SessionTest : SessionTestBase() {
-
-    @Test
-    fun `create and get 2`() {
-        println("Create and get")
-        val session = createSession(0)!!
-        assertEquals("testUser0", session.user.value)
-        assertEquals("testRole0", session.role)
-
-        assertEquals(session, sessionRepository.getById(session.id))
-    }
-
-}
+class SessionTest : SessionTestBase()
