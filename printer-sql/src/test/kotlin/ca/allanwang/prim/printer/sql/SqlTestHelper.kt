d03d006f10bb635ee13aebe45ec99c44e4273f32
Allan Wang <me@allanwang.ca>
2018-11-19 12:12:58 -0500

    Use persistent mem db

--- printer-sql/src/test/kotlin/ca/allanwang/prim/printer/sql/SqlTestHelper.kt
+++ printer-sql/src/test/kotlin/ca/allanwang/prim/printer/sql/SqlTestHelper.kt
@@ -5,14 +5,13 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.transactions.DEFAULT_ISOLATION_LEVEL
 import org.jetbrains.exposed.sql.transactions.ThreadLocalTransactionManager
-import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.koin.dsl.module.Module
 import org.koin.standalone.StandAloneContext.startKoin
-import kotlin.test.fail
 
 /**
  * Extension that binds additional koin modules and sql tables for each test.
@@ -30,25 +29,19 @@ open class SqlExtension(tables: List<Table>,
     }
 
     override fun beforeTestExecution(context: ExtensionContext) {
-        Database.connect("jdbc:h2:mem:test;MODE=MySQL", "org.h2.Driver", "test", "test") {
+        Database.connect("jdbc:h2:mem:test;MODE=MySQL;DB_CLOSE_DELAY=-1", "org.h2.Driver", "test", "test") {
             ThreadLocalTransactionManager(it, DEFAULT_ISOLATION_LEVEL, 1)
         }
-        // New transaction saves transaction to ThreadLocal; accessible through TransactionManager.currentOrNull()
-        val transaction = TransactionManager.manager.newTransaction(DEFAULT_ISOLATION_LEVEL)
-        transaction.debug = debug
-        SchemaUtils.create(*tables)
+        transaction {
+            debug = this@SqlExtension.debug
+            SchemaUtils.create(*tables)
+        }
     }
 
     override fun afterTestExecution(context: ExtensionContext) {
-        val transaction = TransactionManager.currentOrNull() ?: fail("No transaction manager found after sql test")
-        SchemaUtils.drop(*tables)
-        transaction.commit()
-        TransactionManager.resetCurrent(TransactionManager.manager)
-        transaction.currentStatement?.let {
-            if (!it.isClosed) it.close()
-            transaction.currentStatement = null
+        transaction {
+            SchemaUtils.drop(*tables)
         }
-        transaction.closeExecutedStatements()
     }
 
 }
